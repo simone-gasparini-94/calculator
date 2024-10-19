15b1500d47bd8d61912cf2df5235f5e873879478
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2024-10-19 14:06:11 +0200

    added operate function that return one of the functions according to the operator added as an argument

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
+console.log(divide(4, 6));
 
-console.log(multiply(4, 6));
+let a;
+let b;
+let operator;
+
+const operate  = function(a, b, operator) {
+    if (operator === "+") {
+        return add(a, b);
+    };
+    if (operator === "-") {
+        return subtract(a, b);
+    };
+    if (operator === "*") {
+        return multiply(a, b);
+    };
+    if (operator === "-") {
+        return divide(a, b);
+    }
+};
+
+console.log(operate(1, 2, "*"));
